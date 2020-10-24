547c0bc654b91d96041b9acb154b74dc087a35be
Sarac96 <sara.carrara.1996@gmail.com>
2020-10-25 00:17:37 +0200

    Full code, first commit

---
+++ run_analysis.R
@@ -0,0 +1,37 @@
+library(data.table)
+library(dplyr)
+library(tidyr)
+
+# Download files and unzip them
+tempfile <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", tempfile)
+unzip(tempfile, exdir="data")
+file.remove(tempfile)
+
+# Get label names and load the relevant data into R
+labels <- fread("data/UCI HAR Dataset/features.txt", col.names = c("nrow", "feature"))
+trainingset_X <- fread("data/UCI HAR Dataset/train/X_train.txt", sep=" ", col.names = labels$feature)
+testset_X <- fread("data/UCI HAR Dataset/test/X_test.txt", sep=" ", col.names = labels$feature)
+trainingset_Y <- fread("data/UCI HAR Dataset/train/Y_train.txt", sep=" ", col.names = "activitycode")
+testset_Y <- fread("data/UCI HAR Dataset/test/Y_test.txt", sep=" ", col.names = "activitycode")
+subjecttest<- fread("data/UCI HAR Dataset/test/subject_test.txt", sep=" ", col.names = "subject")
+subjecttraining <-fread("data/UCI HAR Dataset/train/subject_train.txt", sep=" ", col.names = "subject")
+activitynames <- fread("data/UCI HAR Dataset/activity_labels.txt", col.names = c("code", "value"))
+
+
+
+#Merge data into one single dataset
+trainingset <- cbind(trainingset_Y, subjecttraining, trainingset_X)
+testset <- cbind(testset_Y, subjecttest, testset_X)
+dtfull <- rbind(trainingset, testset)
+
+# Select only mean and stdev observations
+dtsel <- mutate(dtfull, activityname = activitynames$value[match(activitycode,activitynames$code)])
+dtsel <- select(dtsel, activityname, subject, contains(c("mean()", "std()")))
+
+# Create tidy dataset
+df_tidy <- dtsel %>% group_by(activityname, subject) %>% summarise_all(mean)
+
+# Export tidy dataset
+if (!dir.exists("output")) dir.create("output")
+write.csv(df_tidy,"output/tidy_dataset.csv", row.names = FALSE, na="")
